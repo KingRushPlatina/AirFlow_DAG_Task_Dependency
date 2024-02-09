e93ad7f01cc192d8802b5c70ab8e4cd06ca30094
Amin el ouahidi <amin.elouahidi@niceconsulting.it>
2024-02-09 16:59:08 +0100

    http Call with a Dag

---
+++ dags/Api_dag.py
@@ -0,0 +1,23 @@
+import json;
+from datetime import datetime
+from pickle import FALSE
+from airflow.models import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+
+
+with DAG(
+    dag_id='api_dag',
+    description='Http dag DAG',
+    schedule_interval='@daily',
+    start_date=datetime(2024, 1, 1),
+    catchup=False
+) as dag:
+  task_is_api_active = HttpSensor(
+    task_id='is_api_active',
+    http_conn_id='api_dag',
+    endpoint='module/'
+  )
+  #to test the call from the terminal
+  #airflow tasks test api_dag is_api_active 2024-1-1
--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
+#used for working with rest Apis
+from airflow.providers.http.operators.http import SimpleHttpOperator
 
 from random import randint
 from datetime import datetime
